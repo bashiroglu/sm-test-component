c6457b953028dc113543f6db58624293479405ab
Abdulla Bashiroglu <bashiroglu.abdulla@gmail.com>
2024-10-31 14:26:54 +0400

    new version

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import LoadingCircle from "./components/Notification";
+import LoadingCircle from "./components/loading-circle";
 import useNotification from "./hooks/useNotification";
 
 export { LoadingCircle, useNotification };
